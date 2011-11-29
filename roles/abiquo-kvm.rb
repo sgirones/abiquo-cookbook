fb5885affc0ad6516b8bb9b35c7f94f8f68830a6
Sergio Rubio <rubiojr@frameos.org>
2011-11-29 15:18:50 +0100

    * updated kvm and xen roles

--- roles/abiquo-kvm.rb
+++ roles/abiquo-kvm.rb
@@ -1,9 +1,12 @@
 name "abiquo-kvm"
+description "The role for an Abiquo KVM cloud node"
+run_list "role[abiquo-host]", "recipe[abiquo::kvm]"
 
 override_attributes(
   "abiquo" => {
     "aim" => {
-      "redis_host" => "127.0.0.1"
+      "redis_host" => "127.0.0.1",
+      "qemu_uri" => "qemu+unix:///system"
     },
   }
 )
--- roles/abiquo-xen.rb
+++ roles/abiquo-xen.rb
@@ -1,9 +1,12 @@
 name "abiquo-xen"
+description "The role for an Abiquo Xen cloud node"
+run_list "role[abiquo-host]", "recipe[abiquo::xen]"
 
 override_attributes(
   "abiquo" => {
     "aim" => {
-      "redis_host" => "127.0.0.1"
+      "redis_host" => "127.0.0.1",
+      "qemu_uri" => "xen+unix:///system"
     },
   }
 )
