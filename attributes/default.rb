f3e08f884dc810c41d4a6bd2dfaf5571cc169cd2
Sergio Rubio <rubiojr@frameos.org>
2012-02-02 16:15:30 +0100

    * updated default repos for 2.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['abiquo']['reboot_after_deploy'] = false
-default['repo']['abiquo-ee']['url'] = "http://mirror.bcn.abiquo.com/2.0/"
+default['repo']['abiquo-ee']['url'] = "http://mirror.bcn.abiquo.com/2.0-rolling/"
 default['repo']['abiquo-ee']['key'] = "RPM-GPG-KEY-Abiquo"
 default['repo']['abiquo']['url'] = "http://mirror.abiquo.com/abiquo/2.0/os/x86_64/"
 default['repo']['abiquo']['key'] = "RPM-GPG-KEY-Abiquo"
